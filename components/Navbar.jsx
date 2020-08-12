dfb3f0eba5f4af6e794d07af6cb419fc9220180f
Arun Gawri <luckygawri@gmail.com>
2020-08-12 10:59:38 +0530

    Feature: Navbar And Satallite Page

---
+++ components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div className="col-lg-2">
+      <h3>SpaceX Launch Programs</h3>
+      <div className="card">
+        <div className="card-body bg-white">
+          <h3>Filters</h3>
+          <h3 className="font-weight-light text-center">Launch Year</h3>
+          <div class="d-flex flex-row flex-wrap justify-content-between">
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+            <button className="btn btn-success mx-3">2006</button>
+          </div>
+          <h3 className="font-weight-light text-center">Successful Launch</h3>
+          <button className="btn btn-success">True</button>
+          <h3 className="font-weight-light text-center">Successful Landing</h3>
+          <button className="btn btn-success">True</button>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ components/RocketList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const RocketList = () => {
+  return (
+    <>
+      <div className="col-10">
+        <div className="card">
+          <div className="card-body bg-white">
+            <div className="">img here</div>
+            <h3>FalconSat#1</h3>
+            <h3>Mission Ids:</h3>
+            <ul>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+            </ul>
+            <h3>LaunchYear: 2006</h3>
+            <h3>LaunchYear: 2006</h3>
+            <h3>LaunchYear: 2006</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
