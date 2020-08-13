e0adfebd9b39c9ea241525f1ff6b0a358a5b7afc
Arun Gawri <luckygawri@gmail.com>
2020-08-13 16:03:47 +0530

    Feature: Navbar Layout Set

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,121 +1,109 @@
 import React, { useState } from "react";
 import { Formik, Field } from "formik";
+import { useInput } from "./hooks/useInput";
+import { ButtonGroup, ToggleButton } from "react-bootstrap";
 
 export const Navbar = () => {
+  const years = [
+    "2006",
+    "2007",
+    "2008",
+    "2009",
+    "2010",
+    "2011",
+    "2012",
+    "2013",
+    "2014",
+    "2015",
+    "2016",
+    "2017",
+    "2018",
+    "2019",
+    "2020",
+  ];
+  const [yearValue, setYearValue] = useInput("");
+  const [successfulLaunch, setSuccessfulLaunch] = useInput("");
+  const [successfulLanding, setSuccessfulLanding] = useInput("");
+
   return (
-    <Formik
-      initialValues={{
-        yearValue: "",
-        successfulLaunch: "",
-        successfulLanding: "",
-      }}
-    >
-      {(formik) => (
-        <div className="col-lg-2">
-          <h3>SpaceX Launch Programs</h3>
-          <div className="card">
-            <div className="card-body bg-white">
-              <h3>Filters</h3>
-              <h3 className="font-weight-light text-center">
-                Launch Year
-              </h3>{" "}
-              <div className="d-flex flex-row flex-wrap justify-content-between">
-                <div
-                  className="btn-group btn-group-toggle"
-                  data-toggle="buttons"
+    <div className="col-lg-2">
+      <h3>SpaceX Launch Programs</h3>
+      <div className="card">
+        <div className="card-body bg-white">
+          <h3>Filters</h3>
+          <h3 className="font-weight-light text-center">Launch Year</h3>
+          <ButtonGroup toggle className="row">
+            {years.map((year) => (
+              <React.Fragment key={year}>
+                <ToggleButton
+                  className="col-5 mr-auto ml-1 my-1"
+                  type="radio"
+                  id={year}
+                  value={year}
+                  variant="success"
+                  name="yearValue"
+                  checked={year === yearValue}
+                  onChange={setYearValue}
                 >
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2006" />
-                    2006
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2007" />
-                    2007
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2008" />
-                    2008
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2009" />
-                    2009
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2010" />
-                    2010
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2011" />
-                    2011
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2012" />
-                    2012
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2013" />
-                    2013
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2014" />
-                    2014
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2015" />
-                    2015
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2016" />
-                    2016
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2017" />
-                    2017
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2018" />
-                    2018
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2019" />
-                    2019
-                  </label>
-                  <label className="btn btn-success m-1 col-6">
-                    <Field name="yearValue" type="radio" value="2020" />
-                    2020
-                  </label>
-                </div>
-              </div>
-              <h3 className="font-weight-light text-center">
-                Successful Launch
-              </h3>
-              <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                <label className="btn btn-success m-1 col-6">
-                  <Field name="successfulLaunch" type="radio" value="true" />
-                  True
-                </label>
-                <label className="btn btn-success m-1 col-6">
-                  <Field name="successfulLaunch" type="radio" value="false" />
-                  False
-                </label>
-              </div>
-              <h3 className="font-weight-light text-center">
-                Successful Landing
-              </h3>
-              <div className="btn-group btn-group-toggle" data-toggle="buttons">
-                <label className="btn btn-success m-1 col-6">
-                  <Field name="successfulLanding" type="radio" value="true" />
-                  True
-                </label>
-                <label className="btn btn-success m-1 col-6">
-                  <Field name="successfulLanding" type="radio" value="false" />
-                  False
-                </label>
-              </div>
-            </div>
-          </div>
+                  {year}
+                </ToggleButton>
+              </React.Fragment>
+            ))}
+          </ButtonGroup>
+          <h3 className="font-weight-light text-center">Successful Launch</h3>
+          <ButtonGroup toggle className="row justify-content-between">
+            <ToggleButton
+              className="mr-3"
+              type="radio"
+              id="successfulLaunchTrue"
+              value="true"
+              variant="success"
+              name="successfulLaunch"
+              checked={successfulLaunch === "true"}
+              onChange={setSuccessfulLaunch}
+            >
+              True
+            </ToggleButton>
+            <ToggleButton
+              type="radio"
+              id="successfulLaunchFalse"
+              value="false"
+              variant="success"
+              name="successfulLaunch"
+              checked={successfulLaunch == "false"}
+              onChange={setSuccessfulLaunch}
+            >
+              False
+            </ToggleButton>
+          </ButtonGroup>
+          <h3 className="font-weight-light text-center">Successful Landing</h3>
+          <ButtonGroup toggle className="row justify-content-between">
+            <ToggleButton
+              className="mr-3"
+              type="radio"
+              id="successfulLandingTrue"
+              value="true"
+              variant="success"
+              name="successfulLanding"
+              checked={successfulLanding === "true"}
+              onChange={setSuccessfulLanding}
+            >
+              True
+            </ToggleButton>
+            <ToggleButton
+              type="radio"
+              id="successfulLandingFalse"
+              value="false"
+              variant="success"
+              name="successfulLanding"
+              checked={successfulLanding === "false"}
+              onChange={setSuccessfulLanding}
+            >
+              False
+            </ToggleButton>
+          </ButtonGroup>
         </div>
-      )}
-    </Formik>
+      </div>
+    </div>
   );
 };
