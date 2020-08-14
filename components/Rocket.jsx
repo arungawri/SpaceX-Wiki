a8744076fb7bc25a53f82490ffe5e771f468bea1
Arun Gawri <luckygawri@gmail.com>
2020-08-14 14:11:53 +0530

    Bugs: Rockets Page Layout and Styling

--- components/Rocket.jsx
+++ components/Rocket.jsx
@@ -14,21 +14,45 @@ export const Rocket = ({
       <div className="card my-1">
         <div className="card-body bg-white">
           <div className="text-center">
-            <img src={img_patch_small} width="150" height="150" />
+            <img
+              src={img_patch_small}
+              width="100%"
+              height="100%"
+              style={{ backgroundColor: "#dddddd" }}
+            />
           </div>
-          <h3>
+          <h3 style={{ color: "#00008B" }}>
             {rocket_name}#{flight_number}
           </h3>
-          <h3>Mission Ids:</h3>
-          {!!mission_id ? mission_id[0] : "null"}
+          <h3>
+            Mission Ids:{" "}
+            <span style={{ color: "#00008B", fontWeight: "normal" }}>
+              {!!mission_id[0] ? mission_id[0] : "N/A"}
+            </span>
+          </h3>
           {/* <ul>
             <li>1</li>
             <li>1</li>
             <li>1</li>
           </ul> */}
-          <h3>LaunchYear: {launch_year}</h3>
-          <h3>Successful Launch: {launch_success ? "true" : "false"}</h3>
-          <h3>Successful Landing: {land_success ? "true" : "false"}</h3>
+          <h3>
+            LaunchYear:{" "}
+            <span style={{ color: "#00008B", fontWeight: "normal" }}>
+              {launch_year}
+            </span>
+          </h3>
+          <h3>
+            Successful Launch:{" "}
+            <span style={{ color: "#00008B", fontWeight: "normal" }}>
+              {launch_success ? "true" : "false"}
+            </span>
+          </h3>
+          <h3>
+            Successful Landing:{" "}
+            <span style={{ color: "#00008B", fontWeight: "normal" }}>
+              {land_success ? "true" : "false"}
+            </span>
+          </h3>
         </div>
       </div>
     </div>
